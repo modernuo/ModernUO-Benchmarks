d7b0879475b37260192e484b40218a834f040c60
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2024-04-18 16:38:34 -0700

    Delete old RNG benchmarks

--- Benchmarks/Rng/Program.cs
+++ Benchmarks/Rng/Program.cs
@@ -1,6 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
 using Benchmarks.Benchmarks.Rng;
 
-// var doublefixed = BenchmarkRunner.Run<BenchmarkDoubleVsFixed>();
-// var xoshiro = BenchmarkRunner.Run<BenchmarkXoshiro>();
 var bitpop = BenchmarkRunner.Run<BenchmarkBitPop>();
