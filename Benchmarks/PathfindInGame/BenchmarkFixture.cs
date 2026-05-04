871548484e0b1355b6c40704d6e583aa0bebfdde
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2026-05-03 22:15:21 -0700

    feat: add UOContent bootstrap fixture for in-game pathfinding benchmarks

---
+++ Benchmarks/PathfindInGame/BenchmarkFixture.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using Server;
+using Server.Items;
+using Server.Misc;
+using Server.Tests.Maps;
+
+namespace PathfindInGame;
+
+public static class BenchmarkFixture
+{
+    private static bool _initialized;
+
+    public static void EnsureInitialized()
+    {
+        if (_initialized)
+        {
+            return;
+        }
+
+        Core.ApplicationAssembly = Assembly.GetExecutingAssembly();
+        Core.LoopContext = new EventLoopContext();
+        Core.Expansion = Expansion.EJ;
+
+        ServerConfiguration.Load(true);
+        ServerConfiguration.AssemblyDirectories.Add(Core.BaseDirectory);
+        AssemblyHandler.LoadAssemblies(["Server.dll", "UOContent.dll"]);
+
+        SkillsInfo.Configure();
+        Server.Network.NetState.Configure();
+        TestMapDefinitions.ConfigureTestMapDefinitions();
+
+        World.Configure();
+        Timer.Init(0);
+        RaceDefinitions.Configure();
+        World.Load();
+        World.ExitSerializationThreads();
+        DecayScheduler.Configure();
+
+        _initialized = true;
+    }
+}
